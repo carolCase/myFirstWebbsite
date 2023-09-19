5f385240dadb001b7c264e62898528b2b3b52d9b
carolCase <carolinazona7@gmail.com>
2023-09-19 09:41:33 +0200

    added images

---
+++ logics.js
@@ -0,0 +1,20 @@
+
+
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
+  function Arrays() {
+    var pris = [" Kort hår =300kr", "Långt hår 350kr", "ny frisyr= 400kr"]
+    alert(pris)
+   
+  }
+
+ 
+
+
+
