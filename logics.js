b11174ea4fd49bbe60bbb09f2e39cf998eafa812
carolCase <carolinazona7@gmail.com>
2023-09-20 13:03:49 +0200

    fixed functions i think

--- logics.js
+++ logics.js
@@ -9,7 +9,8 @@ function openNav() {
     document.getElementById("mySidenav").style.width = "0";
   }
   function Arrays() {
-    var pris = [" Kort hår =300kr", "Långt hår 350kr", "ny frisyr= 400kr"]
+    var pris
+     = [" Kort hår =300kr", "Långt hår 350kr", "ny frisyr= 400kr"]
     alert(pris)
    
   }
